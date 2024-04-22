66553bc1bee16339020ea012119ba1fe129e786a
RafliMahesa <muhammadraflimahesa@gmail.com>
2024-04-22 21:11:20 +0700

    Simulation slow subscriber

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ UserCreatedHandler {
         let ten_millis = time::Duration::from_millis(1000);
         let now = time::Instant::now();
 
-        // thread::sleep(ten_millis);
+        thread::sleep(ten_millis);
 
         println!("In Muhammad Rafli Mahesa's Computer [2206828140]. Message received: {:?}", message);
         Ok(())
